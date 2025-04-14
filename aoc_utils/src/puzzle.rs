cde80451794e0dc782f2bac8818157e8c353cfbc
Michael Lefebvre <michael.lefebvre.31@gmail.com>
2025-04-14 06:38:02 -0400

    day2

--- aoc_utils/src/puzzle.rs
+++ aoc_utils/src/puzzle.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 pub fn fetch_puzzle_input(puzzle_number: u8) -> Result<String> {
-    let aoc_token = std::env::var("AOC_TOKEN")?;
+    let aoc_token = std::env::var("AOC_2024_TOKEN")?;
 
     let client = reqwest::blocking::Client::new();
 
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,10 +1,12 @@
 use aoc_utils::fetch_puzzle_input;
 
-fn main() {
-    // let puzzle_input = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9";
-    let puzzle_input = fetch_puzzle_input(2).unwrap();
+fn main() -> anyhow::Result<()> {
+    let puzzle_input = fetch_puzzle_input(2)?;
 
     println!("part1={}", part1(&puzzle_input));
+    println!("part2={}", part2(&puzzle_input));
+
+    Ok(())
 }
 
 fn part1(puzzle_input: &str) -> String {
@@ -19,28 +21,56 @@ fn part1(puzzle_input: &str) -> String {
                 .map(|n| n.parse().unwrap())
                 .collect();
 
-            let deltas = calculate_deltas(&report);
-            same_signs(&deltas) && adjacent_levels(&deltas)
+            is_all_increasing(&report) || is_all_decreasing(&report)
         })
         .count()
         .to_string()
 }
 
-fn calculate_deltas(report: &[i16]) -> Vec<i16> {
-    let mut deltas = vec![0; report.len() - 1];
-    for i in 0..report.len() - 1 {
-        let n1 = report[i];
-        let n2 = report[i + 1];
-        deltas[i] = n2 - n1;
+fn part2(puzzle_input: &str) -> String {
+    puzzle_input
+        .split("\n")
+        .filter(|line| {
+            if line.is_empty() {
+                return false;
+            }
+            let report: Vec<i16> = line
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect();
+
+            create_reports_with_skip(&report)
+                .iter()
+                .any(|r| is_all_increasing(r) || is_all_decreasing(r))
+        })
+        .count()
+        .to_string()
+}
+
+fn create_reports_with_skip(report: &[i16]) -> Vec<Vec<i16>> {
+    let mut reports = vec![];
+    for i in 0..report.len() {
+        let mut report: Vec<i16> = report.to_vec();
+        report.remove(i);
+        reports.push(report);
     }
-    deltas
+    reports
 }
 
-fn same_signs(deltas: &[i16]) -> bool {
-    let is_positive = deltas[0].is_positive();
-    deltas.iter().all(|n| n.is_positive() == is_positive)
+fn is_all_increasing(report: &[i16]) -> bool {
+    is_all_inner(report, |a, b| b - a)
 }
 
-fn adjacent_levels(deltas: &[i16]) -> bool {
-    deltas.iter().all(|n| n.abs() >= 1 && n.abs() <= 3)
+fn is_all_decreasing(report: &[i16]) -> bool {
+    is_all_inner(report, |a, b| a - b)
+}
+
+fn is_all_inner(report: &[i16], diff_fn: fn(i16, i16) -> i16) -> bool {
+    for i in 1..report.len() {
+        let diff = diff_fn(report[i - 1], report[i]);
+        if !(1..=3).contains(&diff) {
+            return false;
+        }
+    }
+    true
 }
